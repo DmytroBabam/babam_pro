b3e948bdaba70fc9b000591a599799552abdf82c
Dmytro Pokorskyi <dpokor66424@gmail.com>
2023-11-01 04:22:26 +0100

    init

---
+++ src/App.js
@@ -0,0 +1,43 @@
+ import React, { useState } from "react";
+
+function App() {
+const [likes, setLikes] =  useState(0)
+
+function Increment() {
+    setLikes ( likes + 1 )
+const numberX=Number(1)
+const numberY=Number(1)
+
+
+//......
+
+
+  
+
+    let whoLikes = numberX===numberY ? 'Likes' : 'not likes'
+console.log("Dima " + (whoLikes || "hiiiii") + ' Kenan')
+
+}
+
+function Decrement() {
+
+  setLikes ( likes - 1 )
+console.log(likes);
+}
+
+  return (
+    <div className="App"> 
+
+<h1>{likes}</h1>
+<div style={{backgroundColor:'red',margin:'20px',padding:'50px', textAlign:'center',bottom:0,flexDirection:'column',flex:1}}>
+  <button onClick={Increment} style={{color:'green',borderRadius:'5px',height:'50px', width:'300px'}}>Increment</button>  
+  <button onClick={Decrement} style={{color:'red',borderRadius:'5px'}}>Decrement</button>
+
+</div>
+    
+
+    </div>
+  );
+  }
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from "./App"
+import Kenan from "./Kenan"
+
+ReactDOM.render(
+  <App/>,
+    document.getElementById('dima')
+
+);
+ReactDOM.render(
+  <Kenan/>,
+    document.getElementById('kenan')
+
+);
