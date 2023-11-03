09073459c0a27076e3395f12ffe63bd18a612086
Dmytro Pokorskyi <149539762+DmytroBabam@users.noreply.github.com>
2023-11-03 07:01:55 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,50 +1,18 @@
-import React, {useState} from "react";
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import HomePage from "./Pages/homepage";
+import NewPage from "./Pages/newpage";
 
 function App() {
-    const [likes, setLikes] = useState(0)
-
-    function Increment() {
-        setLikes(likes + 1)
-        const numberX = Number(1)
-        const numberY = Number(1)
-
-
-//......
-
-
-        let whoLikes = numberX === numberY ? 'Likes' : 'not likes'
-        console.log("Dima " + (whoLikes || "hiiiii") + ' Kenan')
-
-    }
-
-    function Decrement() {
-
-        setLikes(likes - 1)
-        console.log(likes);
-    }
-
     return (
-        <div className="App">
-
-            <h1>{likes}</h1>
-            <h2>Test</h2>
-            <div style={{
-                backgroundColor: 'red',
-                margin: '20px',
-                padding: '50px',
-                textAlign: 'center',
-                bottom: 0,
-                flexDirection: 'column',
-                flex: 1
-            }}>
-                <button onClick={Increment}
-                        style={{color: 'green', borderRadius: '5px', height: '50px', width: '300px'}}>Increment
-                </button>
-                <button onClick={Decrement} style={{color: 'red', borderRadius: '5px'}}>Decrement</button>
-
-            </div>
-
-
+        <div>
+            <header>
+                <h2><link rel="stylesheet" href="/newpage" />NewPage</h2>
+            </header>
+            <Routes>
+                <Route path="/newpage" element={<NewPage />} />
+                <Route path="/" element={<HomePage />} />
+            </Routes>
         </div>
     );
 }
